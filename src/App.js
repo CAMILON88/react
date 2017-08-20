304dccfc61586e484a31ee4a0a759770d2dd183a
CAMILON88 <camilonino88@gmail.com>
2017-08-19 22:43:46 -0500

    Version Inicial de React

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+
+          <h1>Mi primera aplicación</h1>
+        </div>
+        <p>
+          Esta es mi primera aplicación en React, está padrísimo!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
